9c41eadd6f22b951c0158586023b178a810075f9
Emily Randall <ej.randall@me.com>
2021-06-01 08:46:40 +0100

    snowplow-simple

---
+++ src/Products.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+
+const HOME_GARDEN = 'Home and Garden';
+const UTILITY = 'Utility';
+
+export default function Products({ setCart, cart }) {
+  const [products] = useState([
+    {
+      category: UTILITY,
+      name: 'AA Battery',
+      cost: 2.99,
+      image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ5-QAul_NfAs-s0XW9M087xWyPOGWvbfYjmqSl0QXabZRSYoid47i7kISiAteyIh0YOci5mtQ&usqp=CAc',
+    },
+    {
+      category: HOME_GARDEN,
+      name: 'Blanket',
+      cost: 19.99,
+      image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSpwdYDmUL_ZEqhLV7ZWRdQAU7DGcGaxtCt7SrTlL9umrQs2Un7rj9Nbb9Vq01RtEfA0eAVmdt-&usqp=CAc',
+    },
+    {
+      category: UTILITY,
+      name: 'Hammer',
+      cost: 9.99,
+      image:
+        'https://www.oxtools.co.uk/media/catalog/product/cache/b6a881ddf87baeb431416117cd7f8416/o/x/ox-t085116.jpg',
+    }
+  ]);
+
+  const addToCart = (product) => {
+    let newCart = [...cart];
+    let itemInCart = newCart.find(
+      (item) => product.name === item.name
+    );
+    if (itemInCart) {
+      itemInCart.quantity++;
+    } else {
+      itemInCart = {
+        ...product,
+        quantity: 1,
+      };
+      newCart.push(itemInCart);
+    }
+    setCart(newCart);
+  };
+
+  const [category, setCategory] = useState(HOME_GARDEN);
+
+  const getProductsInCategory = () => {
+    return products.filter(
+      (product) => product.category === category
+    );
+  };
+
+  return (
+    <>
+      <h1>Products</h1>
+      Select a category <br></br><br></br>
+      <select onChange={(e) => setCategory(e.target.value)}>
+        <option value={HOME_GARDEN}>{HOME_GARDEN}</option>
+        <option value={UTILITY}>{UTILITY}</option>
+      </select>
+      <div className="products">
+        {getProductsInCategory().map((product, idx) => (
+          <div className="product" key={idx}>
+            <h3>{product.name}</h3>
+            <h4>£{product.cost}</h4>
+            <img src={product.image} alt={product.name} />
+            <button onClick={() => addToCart(product)}>
+              Add to Cart
+            </button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
